3f61f8f65a6436db267594e8a692e1820ce09a35
Vinay-webDev <justchatting3000@gmail.com>
2024-11-01 23:34:12 +0530

    1deleteCharactersToMakeFancyString(1957)✅

---
+++ 1fancystring.py
@@ -0,0 +1,81 @@
+"""
+1957. Delete Characters to Make Fancy String
+Easy
+Topics
+Companies
+Hint
+A fancy string is a string where no three consecutive characters are equal.
+
+Given a string s, delete the minimum possible number of characters from s to make it fancy.
+
+Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+
+Example 1:
+
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+Example 2:
+
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+Example 3:
+
+Input: s = "aab"
+Output: "aab"
+Explanation: No three consecutive characters are equal, so return "aab".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists only of lowercase English letters.
+"""
+
+s1 = "leeetcode"
+s2 = "aaabaaaa"
+"""
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        string = ""
+        for i in range(len(s)):
+            if (s[i] == s[i+1]):
+                if (s[i+1] == s[i+2]):
+                    string += ""
+                else:
+                    string += s[i]
+            else:
+                string += s[i]
+        
+        print(string)
+        return string
+
+sol = Solution()
+
+sol.makeFancyString(s1)
+sol.makeFancyString(s2)
+"""
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        string = ""
+        
+        for i in range(len(s)):
+            if (i + 1 < len(s) and i + 2 < len(s) and s[i] == s[i+1] == s[i+2]):
+                continue
+            else:
+                string += s[i]
+        
+        print(string)
+        return string
+
+sol = Solution()
+
+sol.makeFancyString(s1)
+sol.makeFancyString(s2)
