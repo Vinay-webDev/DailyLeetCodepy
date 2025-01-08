50268631dc4762f6b7c98a018fe07583dfb6bfb8
Vinay-webDev <justchatting3000@gmail.com>
2025-01-08 15:04:18 +0530

    #13.1

--- 13.py
+++ 13.py
@@ -1,8 +1,5 @@
 """07/01/25"""
-words1 = ["mass","as","hero","superhero"]
-words2 = ["leetcode","et","code"]
-words3 = ["blue","green","bu"]
-words4 = ["leetcoder","leetcode","od","hamlet","am"]
+
 class Solution:
     def stringMatching(self, words):
         n = len(words)
@@ -12,12 +9,30 @@ class Solution:
                 if i != j and words[j] in words[i]:
                     res.add(words[j])
         return [*res]
+# sol = Solution()
+# print(sol.stringMatching(words1))  #['hero', 'as']
+# print(sol.stringMatching(words2))  #['et', 'code']
+# print(sol.stringMatching(words3))  #[]
+# print(sol.stringMatching(words4))  #['leetcode', 'od', 'am']
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+words1 = ["mass","as","hero","superhero"]
+words2 = ["leetcode","et","code"]
+words3 = ["blue","green","bu"]
+words4 = ["leetcoder","leetcode","od","hamlet","am"]
+class Solution:
+    def stringMatching(self, words):
+        N = len(words)
+        res = []
+        for i in range(N):
+            for j in range(N):
+                if i != j and words[j] in words[i]:
+                    res.append(words[j])
+        return res
 sol = Solution()
-print(sol.stringMatching(words1))  #['hero', 'as']
-print(sol.stringMatching(words2))  #['et', 'code']
-print(sol.stringMatching(words3))  #[]
-print(sol.stringMatching(words4))  #['leetcode', 'od', 'am']
-
-
+print(sol.stringMatching(words1))   #['hero', 'as']
+print(sol.stringMatching(words2))   #['et', 'code']
+print(sol.stringMatching(words3))   #[]
+print(sol.stringMatching(words4))   #['leetcode', 'od', 'am']
 
 
