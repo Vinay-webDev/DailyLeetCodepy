7b1e8fa6319bbbecb574d79486299aed4bf0723d
Vinay-webDev <justchatting3000@gmail.com>
2025-01-08 16:03:12 +0530

    #14

---
+++ 14.py
@@ -0,0 +1,27 @@
+"""08/01/25"""
+words1 = ["a","aba","ababa","aa"]
+words2 = ["pa","papa","ma","mama"]
+words3 = ["abab","ab"]
+words4 = ["a","abb"]
+class Solution:
+    def countPrefixSuffixPairs(self, words):
+        N = len(words)
+        count = 0
+        for i in range(N):
+            for j in range(i + 1, N):
+                str1 = words[i]
+                length = len(str1)
+                str2 = words[j]
+                if str1 in str2[:length] and str1 in str2[-length:]:
+                    count += 1
+        return count
+sol = Solution()
+print(sol.countPrefixSuffixPairs(words1))   #4
+print(sol.countPrefixSuffixPairs(words2))   #2
+print(sol.countPrefixSuffixPairs(words3))   #0
+print(sol.countPrefixSuffixPairs(words4))   #0
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-queries = [[0, 4], [2, 4], [9, 12]]
-ans = [False] * len(queries)
-print(ans) #[False, False, False]
+# queries = [[0, 4], [2, 4], [9, 12]]
+# ans = [False] * len(queries)
+# print(ans) #[False, False, False]
+
+word = "python"
+print(word[:1])             #p
+print(word[len(word) - 1:]) #n
+
+
+
+
+
+
+
