9963e2a539a4d28917b2d7d13e4920149c4838b1
Vinay-webDev <justchatting3000@gmail.com>
2025-01-10 23:53:39 +0530

    #16

--- 16.py
+++ 16.py
@@ -1,10 +1,7 @@
 """10/01/25"""
-words1a = ["amazon","apple","facebook","google","leetcode"]
-words2a = ["e","o"]
-words1b = ["amazon","apple","facebook","google","leetcode"]
-words2b = ["l","e"]
-words1c = ["amazon","apple","facebook","google","leetcode"]
-words2c = ["lo","eo"]
+
+
+
 class Solution:
     def wordSubsets(self, words1, words2):
         res = []
@@ -21,14 +18,40 @@ class Solution:
                     res.append(words1[i])
                     check = 0
         return res
+# sol = Solution()
+# print(sol.wordSubsets(words1a, words2a))  #["facebook","google","leetcode"]
+# print(sol.wordSubsets(words1b, words2b))  #['apple', 'google', 'leetcode']
+# print(sol.wordSubsets(words1c, words2c))  #[]  ❌WRONG ANSWER❌
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+words1a = ["amazon","apple","facebook","google","leetcode"]
+words2a = ["e","o"]
+words1b = ["amazon","apple","facebook","google","leetcode"]
+words2b = ["l","e"]
+words1c = ["amazon","apple","facebook","google","leetcode"]
+words2c = ["lo","eo"]
+class Solution:
+    def wordSubsets(self, words1, words2):
+        from collections import Counter, defaultdict
+        count2 = defaultdict(int)
+        for w in words2:
+            countw = Counter(w)
+            for c, cnt in countw.items():
+                count2[c] = max(count2[c], cnt)
+        res = []
+        for w in words1:
+            countw = Counter(w)
+            flag = True
+            for c, cnt in count2.items():
+                if countw[c] < cnt:
+                    flag = False
+                    break
+            if flag:
+                res.append(w)
+        return res
 sol = Solution()
 print(sol.wordSubsets(words1a, words2a))  #["facebook","google","leetcode"]
 print(sol.wordSubsets(words1b, words2b))  #['apple', 'google', 'leetcode']
-print(sol.wordSubsets(words1c, words2c))  #[]  ❌WRONG ANSWER❌
-
-
-
-
+print(sol.wordSubsets(words1c, words2c))  #['google', 'leetcode']  
 
 
 
